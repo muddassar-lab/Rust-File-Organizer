8db7a61b90fbe4afe792a1a2f47f1d6bd208796d
muddassar <muddassarsaleem89@gmail.com>
2025-03-16 01:00:55 +0500

    Added refactoring.

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,7 +1,9 @@
 mod init;
 mod process;
 mod result;
+mod runner;
 
 pub use init::{InitResult, initialize_app};
 pub use process::spawn_processing_thread;
 pub use result::handle_organization_result;
+pub use runner::run_app;
